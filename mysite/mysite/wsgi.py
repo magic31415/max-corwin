939c021c5fa503a64280981a529da41851cb049f
Max Corwin <maxcorwin@yahoo.com>
2016-12-09 03:30:02 -0500

    change wsgi

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+import django.core.handlers.wsgi
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
+
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+application = django.core.handlers.wsgi.WSGIHandler()
