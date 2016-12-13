61401866e102a04fbaac144aff2f78edde8410b5
Max Corwin <maxcorwin@yahoo.com>
2016-12-13 00:37:22 -0500

    add twilio

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import SignupForm
+from twilio.rest import TwilioRestClient
 
 def index(request):
     if request.method == 'POST':
@@ -8,8 +9,14 @@ def index(request):
         if form.is_valid():
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
-            with open('records.txt', 'w') as f:
-                f.write(name + '\t' + email +'\n')
+            ACCOUNT_SID = "AC1ff17efc8a25502eacba5b59825d090f" 
+            AUTH_TOKEN = "4321056c12f4ba6de2982cb2835504dc"    
+            client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)     
+            client.messages.create(
+                to="+15163535462", 
+                from_="+15162104088", 
+                body= '\n' + name + '\n' + email,  
+            ) 
             return HttpResponseRedirect('/thanks/')
     else:   
         form = SignupForm()
